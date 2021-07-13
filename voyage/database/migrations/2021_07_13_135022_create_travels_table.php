9ed0d7f3f65219cd9ffacddbc8c2716ba533d6cb
AndreaDex <desiderio.andrea@outlook.it>
2021-07-13 16:25:15 +0200

    made migration

---
+++ voyage/database/migrations/2021_07_13_135022_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('country', 50);
+            $table->string('location', 100);
+            $table->text('info')->nullable();
+            $table->date('departure')->nullable();
+            $table->tinyInteger('night')->nullable();
+            $table->decimal('price', 8, 2)->nullable();
+            $table->string('poster')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
