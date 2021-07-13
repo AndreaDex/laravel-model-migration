83e32627ae953467682fae07bc199ff64bcc29bc
AndreaDex <desiderio.andrea@outlook.it>
2021-07-13 17:37:16 +0200

    made card

--- voyage/resources/views/viaggi.blade.php
+++ voyage/resources/views/viaggi.blade.php
@@ -3,5 +3,24 @@
 @section('title', 'Viaggi')
 
 @section('content')
-<h1>Viaggi</h1>
+<div class="gallery">
+
+    @foreach($travels as $travel)
+
+    <div class="card">
+        <div class="poster">
+            <img src="{{$travel->poster}}" alt="">
+        </div>
+        <h2>{{$travel->country}}</h2>
+        <h3>{{$travel->location}}</h3>
+        <p>Partenza : {{$travel->departure}}</p>
+        <p>{{$travel->night}} Notti</p>
+        <p>Prezzo : {{$travel->price}} &euro;</p>
+        <p>{{$travel->info}}</p>
+    </div>
+
+    @endforeach
+</div>
+
+
 @endsection
